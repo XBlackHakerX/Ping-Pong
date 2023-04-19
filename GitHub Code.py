3f4b695ee7d43adf6d36521ecfda5b58762d97f2
XBlackHakerX <130580617+XBlackHakerX@users.noreply.github.com>
2023-04-19 18:54:17 +0300

    Update GitHub Code.py
    v1.1

--- GitHub Code.py
+++ GitHub Code.py
@@ -1,4 +1,4 @@
-#GitHub Code
+
 from pygame import *
 
 class GameSprite(sprite.Sprite):
@@ -15,15 +15,35 @@ class GameSprite(sprite.Sprite):
 class Player(GameSprite):
     def update(self):
         keys_pressed = key.get_pressed()    
-        if keys_pressed[K_w] and self.rect.x > 0:
-            self.rect.x -= self.speed
-        if keys_pressed[K_s] and self.rect.x < 630:
-            self.rect.x += self.speed
+        if keys_pressed[K_w] and self.rect.y > 0:
+            self.rect.y -= self.speed
+        if keys_pressed[K_s] and self.rect.y < 430:
+            self.rect.y += self.speed
 
 class Player2(GameSprite):
     def update(self):
         keys_pressed = key.get_pressed()    
-        if keys_pressed[Up] and self.rect.x > 0:
-            self.rect.x -= self.speed
-        if keys_pressed[Down] and self.rect.x < 630:
-            self.rect.x += self.speed
+        if keys_pressed[K_UP] and self.rect.y > 0:
+            self.rect.y -= self.speed
+        if keys_pressed[K_DOWN] and self.rect.y < 430:
+            self.rect.y += self.speed
+
+window = display.set_mode((700, 500))
+sp_Player = Player('Kapibaara.jpg', 0, 420, 65, 65, 10)
+sp_Player2 = Player2('Kapibaara.jpg', 635, 420, 65, 65, 10)
+display.set_caption("Kapibara")
+background = transform.scale(image.load('Kapibaara.jpg'), (700, 500))
+
+game = True
+clock = time.Clock()
+while game:
+    for e in event.get():
+        if e.type == QUIT:
+            game = False
+    window.blit(background, (0,0))
+    sp_Player.update()
+    sp_Player.reset()
+    sp_Player2.update()
+    sp_Player2.reset()
+    display.update()
+    clock.tick(60)
